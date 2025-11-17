8a10ebc15a94d82aaafabd6d76f113f263af674e
BinaryFoxer <19971551256@163.com>
2025-11-17 13:20:10 +0800

    stop tracking models

--- src/models/customResNet.py
+++ src/models/customResNet.py
@@ -6,11 +6,79 @@ from torch.optim.lr_scheduler import StepLR, CosineAnnealingLR
 from torchvision import datasets, transforms
 import torchvision.models as models
 
-class customResNet():
-    '''
-    Custom ResNet for CIFAR-10.
-    '''
+class CustomCIFAR10Model(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CustomCIFAR10Model, self).__init__()
+        
+        self.features = nn.Sequential(
+            # 初始卷积层
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            
+            # 残差块风格的多层卷积
+            self._make_layer(64, 64, 2),
+            self._make_layer(64, 128, 2, stride=2),
+            self._make_layer(128, 256, 2, stride=2),
+            self._make_layer(256, 512, 2, stride=2),
+            
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
     
+    def _make_layer(self, in_channels, out_channels, blocks, stride=1):
+        layers = []
+        # 下采样块
+        layers.append(ResidualBlock(in_channels, out_channels, stride))
+        
+        # 恒等块
+        for _ in range(1, blocks):
+            layers.append(ResidualBlock(out_channels, out_channels))
+        
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        #  shortcut连接
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+    
+    def forward(self, x):
+        residual = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        out += self.shortcut(residual)
+        out = self.relu(out)
+        
+        return out
 
 
 
